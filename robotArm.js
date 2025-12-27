640a384a23786e164c3b86f5b84ea96460b0ea57
syamie66 <syamielamr@gmail.com>
2025-12-27 22:20:34 +0800

    create gripper

--- robotArm.js
+++ robotArm.js
@@ -2,23 +2,31 @@
 
 var canvas, gl, program;
 
-var theta = [0, 0, 15];
-var BASE = 0, LOWER_ARM = 1, UPPER_ARM = 2;
+// --- JOINT VARIABLES ---
+// [Updated] Added 4th value for Wrist Rotation
+var theta = [0, 0, 15, 0]; 
+var BASE = 0, LOWER_ARM = 1, UPPER_ARM = 2, WRIST = 3;
 
-// Dimensions to match your reference image
+// --- DIMENSIONS ---
 var BASE_H = 1.0, BASE_W = 5.0; 
 var LOWER_H = 4.0, LOWER_W = 1.0;
 var UPPER_H = 3.5, UPPER_W = 1.0;
 
-var TABLE_W = 15.0, TABLE_H = 0.2, TABLE_D = 8.0;
-var LEG_H = 5.0, LEG_W = 0.3;
-
+// --- MATRICES ---
 var modelViewMatrix;
 var modelViewMatrixLoc, vColorLoc;
+var viewMatrix; 
 
 var points = [];
 var colors = [];
 
+// --- VARIABLES ---
+var gripperGap = 0.2;         
+var isObjectCaught = false;   
+var objectPosition = vec3(5.0, -1.4, 0.0); 
+var trueWristPosition = vec3(0, 0, 0); 
+
+// Vertices & Colors (Standard)
 var vertices = [
     vec3(-0.5, -0.5,  0.5), vec3(-0.5,  0.5,  0.5),
     vec3( 0.5,  0.5,  0.5), vec3( 0.5, -0.5,  0.5),
@@ -91,78 +99,151 @@ window.onload = function init() {
 
     modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
     
-    // Zoom out enough to see the legs (-12 to 12)
     var projectionMatrix = ortho(-15, 15, -10, 10, -50, 50);
     gl.uniformMatrix4fv(gl.getUniformLocation(program, "projectionMatrix"), false, flatten(projectionMatrix));
 
+    // Sliders
     document.getElementById("slider1").oninput = e => theta[BASE] = e.target.value;
     document.getElementById("slider2").oninput = e => theta[LOWER_ARM] = e.target.value;
     document.getElementById("slider3").oninput = e => theta[UPPER_ARM] = e.target.value;
+    // [Updated] Wrist Slider
+    document.getElementById("slider4").oninput = e => theta[WRIST] = e.target.value;
+
+    // Button Logic
+    var btn = document.getElementById("grabBtn");
+    if(btn) {
+        btn.onclick = function() {
+            if(isObjectCaught) {
+                // RELEASE
+                isObjectCaught = false; 
+                gripperGap = 0.2; 
+                objectPosition = vec3(trueWristPosition[0], -1.4, trueWristPosition[2]);
+                console.log("Object PLACED at: ", objectPosition);
+            } else {
+                // GRASP
+                var dist = Math.sqrt(
+                    Math.pow(trueWristPosition[0] - objectPosition[0], 2) +
+                    Math.pow(trueWristPosition[2] - objectPosition[2], 2)
+                );
+                
+                if (dist < 2.5) { 
+                    isObjectCaught = true;
+                    gripperGap = 0.0; 
+                    console.log("Object PICKED!");
+                } else {
+                    alert("Missed! Distance: " + dist.toFixed(2) + ". Move closer.");
+                }
+            }
+        };
+    }
 
     render();
 };
 
+// --- GRIPPER DRAWING ---
+function drawGripper(rootMatrix) {
+    var railColor = vec4(0.2, 0.2, 0.2, 1.0);   // Dark Grey
+    
+    // [Updated] Pink Color for Fingers
+    var fingerColor = vec4(1.0, 0.2, 0.6, 1.0); // Hot Pink
+
+    // Wrist Rail
+    var rail = mult(rootMatrix, translate(0, 0.2, 0)); 
+    modelViewMatrix = rail;
+    drawSolidCube(2.4, 0.4, 0.4, railColor); 
+
+    // Left Finger
+    var leftPos = -0.6 - gripperGap; 
+    var leftFinger = mult(rootMatrix, translate(leftPos, 1.0, 0)); 
+    modelViewMatrix = leftFinger;
+    drawSolidCube(0.3, 1.6, 0.8, fingerColor); 
+
+    // Right Finger
+    var rightPos = 0.6 + gripperGap;
+    var rightFinger = mult(rootMatrix, translate(rightPos, 1.0, 0)); 
+    modelViewMatrix = rightFinger;
+    drawSolidCube(0.3, 1.6, 0.8, fingerColor); 
+}
+
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // 1. CAMERA VIEW (Top-down tilted view)
+    // 1. CAMERA
     var rX = rotateX(30);
     var rY = rotateY(25);
-    var viewMatrix = mult(rX, rY); 
+    viewMatrix = mult(rX, rY); 
 
-    // 2. DRAW TABLE (Black Top)
-    // We create a specific matrix for the table
+    // 2. SCENE
     var tableModelMatrix = mult(viewMatrix, translate(0, -2.0, 0));
     modelViewMatrix = tableModelMatrix;
     drawSolidCube(15.0, 0.2, 8.0, vec4(0, 0, 0, 1)); 
 
-    // 3. DRAW LEGS (Grey)
-    var lx = 7.0, lz = 3.5;
     var grey = vec4(0.8, 0.8, 0.8, 1.0);
-    
-    // Legs move relative to the viewMatrix
     var legPositions = [
-        translate(-lx, -4.5,  lz),
-        translate( lx, -4.5,  lz),
-        translate(-lx, -4.5, -lz),
-        translate( lx, -4.5, -lz)
+        translate(-7.0, -4.5,  3.5), translate( 7.0, -4.5,  3.5),
+        translate(-7.0, -4.5, -3.5), translate( 7.0, -4.5, -3.5)
     ];
-
     for(var i=0; i<4; i++) {
         modelViewMatrix = mult(viewMatrix, legPositions[i]);
         drawSolidCube(0.4, 5.0, 0.4, grey);
     }
 
-    // 4. DRAW ROBOT (The Missing Part)
-    // We move the robot root to be just ABOVE the table surface (y = -1.9)
+    // 3. ROBOT
     var robotXlate = translate(0, -1.9, 0); 
     var robotRoot = mult(viewMatrix, robotXlate);
     
-    // Apply Base Rotation (Slider 1)
+    // Base
     robotRoot = mult(robotRoot, rotateY(theta[BASE]));
-
-    // --- BASE CUBE ---
-    // Offset the cube upward by half its height (0.5) so its bottom touches the table
     modelViewMatrix = mult(robotRoot, translate(0, 0.5, 0));
     drawRobotCube(5.0, 1.0, 2.0);
 
-    // --- LOWER ARM ---
-    // Start from top of base (y = 1.0)
+    // Lower Arm
     var lowerArmRoot = mult(robotRoot, translate(0, 1.0, 0));
     lowerArmRoot = mult(lowerArmRoot, rotateZ(theta[LOWER_ARM]));
-    
-    // Move up by half height (2.0) to draw
     modelViewMatrix = mult(lowerArmRoot, translate(0, 2.0, 0));
     drawRobotCube(1.0, 4.0, 1.0);
 
-    // --- UPPER ARM ---
-    // Start from top of lower arm (y = 4.0)
+    // Upper Arm
     var upperArmRoot = mult(lowerArmRoot, translate(0, 4.0, 0));
     upperArmRoot = mult(upperArmRoot, rotateZ(theta[UPPER_ARM]));
-    
-    // Move up by half height (1.75) to draw
     modelViewMatrix = mult(upperArmRoot, translate(0, 1.75, 0));
     drawRobotCube(1.0, 3.5, 1.0);
+
+    // --- GRIPPER WITH ROTATION ---
+    // Move to top of upper arm
+    var gripperPos = mult(upperArmRoot, translate(0, 3.5, 0));
+    // [Updated] Apply Wrist Rotation (RotateY spins the wrist)
+    var gripperRoot = mult(gripperPos, rotateY(theta[WRIST]));
+    
+    drawGripper(gripperRoot);
+
+    // --- LOGIC: SHADOW MATRIX ---
+    // Re-calculate world position logic (ignoring rotation for distance check)
+    var wRoot = translate(0, -1.9, 0);
+    wRoot = mult(wRoot, rotateY(theta[BASE]));
+    var wLower = mult(wRoot, translate(0, 1.0, 0));
+    wLower = mult(wLower, rotateZ(theta[LOWER_ARM]));
+    var wUpper = mult(wLower, translate(0, 4.0, 0));
+    wUpper = mult(wUpper, rotateZ(theta[UPPER_ARM]));
+    var wGripper = mult(wUpper, translate(0, 3.5, 0));
+    
+    if(wGripper[0].length === undefined) {
+        trueWristPosition = vec3(wGripper[12], wGripper[13], wGripper[14]);
+    } else {
+        trueWristPosition = vec3(wGripper[0][3], wGripper[1][3], wGripper[2][3]);
+    }
+
+    // --- DRAW OBJECT ---
+    if (isObjectCaught) {
+        // [Updated] Picked Object follows Gripper Rotation
+        var heldObject = mult(gripperRoot, translate(0, 1.0, 0)); 
+        modelViewMatrix = heldObject;
+        drawSolidCube(1.0, 1.0, 1.0, vec4(1, 0, 0, 1)); 
+    } else {
+        var freeObject = mult(viewMatrix, translate(objectPosition[0], objectPosition[1], objectPosition[2]));
+        modelViewMatrix = freeObject;
+        drawSolidCube(1.0, 1.0, 1.0, vec4(1, 0, 0, 1)); 
+    }
 
     requestAnimationFrame(render);
 }
